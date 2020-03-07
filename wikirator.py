607bc7c9b8f9531cb4f5d26f96527ceac4875b9f
Aleksey Semenov <7.on.off@bk.ru>
2020-03-07 13:13:00 +0300

    iterator

--- wikirator.py
+++ wikirator.py
@@ -1,14 +1,24 @@
 import json
+
 import requests
+
 import wikipedia_api.api as api
 
 
 class GetUrl:
     def __init__(self, filepath):
         with open(filepath, encoding='utf8') as f:
-            countries = json.load(f)
-            session = requests.Session()
-            for item in countries:
-                search_page = item['name']['common']
-                print(api.get_url(api.get_ids(search_page, session), session))
-            session.close()
+            self.countries = [item['name']['common'] for item in json.load(f)]
+            self.start = -1
+            self.end = len(self.countries)
+            self.session = requests.Session()
+            self.session.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+        if self.start == self.end:
+            raise StopIteration
+        return api.get_url(api.get_ids(self.countries[self.start], self.session), self.session)
